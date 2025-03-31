ff12e1e0387d3002904f60c84ed12032b9e905c2
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-03-31 16:34:44 +0200

    add MessageHandlerBatchDelay

---
+++ kafka_message-handler-batch-delay.go
@@ -0,0 +1,32 @@
+// Copyright (c) 2025 Benjamin Borbe All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package kafka
+
+import (
+	"context"
+
+	"github.com/IBM/sarama"
+	"github.com/bborbe/errors"
+	libtime "github.com/bborbe/time"
+)
+
+// MessageHandlerBatchDelay returns a MessageHandlerBatch
+// that sleep for the given duration after each consume
+// this enabled the consumer to get more messages per consume
+func MessageHandlerBatchDelay(
+	messageHandlerBatch MessageHandlerBatch,
+	waiterDuration libtime.WaiterDuration,
+	delay libtime.Duration,
+) MessageHandlerBatch {
+	return MessageHandlerBatchFunc(func(ctx context.Context, messages []*sarama.ConsumerMessage) error {
+		if err := messageHandlerBatch.ConsumeMessages(ctx, messages); err != nil {
+			return errors.Wrapf(ctx, err, "consume messages failed")
+		}
+		if err := waiterDuration.Wait(ctx, delay); err != nil {
+			return errors.Wrapf(ctx, err, "wait failed")
+		}
+		return nil
+	})
+}
